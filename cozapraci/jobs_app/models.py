a830e11f4abd23159cbc2181306e48fbf7bc23e8
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2022-03-30 13:35:27 +0200

    structure fixed, basic functionality applied

--- cozapraci/jobs_app/models.py
+++ cozapraci/jobs_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    job_name = models.CharField()
+    job_name = models.CharField(max_length=30)
     stereotype = models.TextField()
     reality = models.TextField()
     day_in_life = models.TextField()
