0a034860984abffbe72d6da4d498520791d587d5
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2022-03-30 12:14:37 +0200

    created basic files and set up models, incorrect structure

--- jobs_app/models.py
+++ jobs_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    job_name = models.CharField()
+    stereotype = models.TextField()
+    reality = models.TextField()
+    day_in_life = models.TextField()
+    income = models.IntegerField()
+    education = models.TextField()
+    others = models.TextField()
+    name = models.CharField(max_length=30)
+
--- jobs_app/views.py
+++ jobs_app/views.py
@@ -1,3 +1,25 @@
+from dataclasses import fields
 from django.shortcuts import render
+from django.urls import reverse,reverse_lazy
+from django.views.generic import TemplateView, CreateView,DetailView, FormView,ListView,UpdateView,DeleteView
+from jobs_app.models import Job
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'jobs_app/home.html'
+
+class ThankYouView(TemplateView):
+    template_name = 'jobs_app/thank_you.html'
+
+class JobCreateView(CreateView):
+    model = Job
+    fields = "__all__"
+    success_url = reverse_lazy("jobs_app:thank_you")
+
+class JobListView(ListView):
+    model = Job
+    query_set = Job.objects.order_by("job_name")
+    context_object_name = 'jobs_list'
+
+class JobDetailView(DetailView):
+    model = Job
