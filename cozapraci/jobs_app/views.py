670ff49e986248e3935ab3e8278cd116081dfcb4
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2022-04-16 12:33:45 +0200

    improved random - whole database is not loaded into the list

--- cozapraci/jobs_app/views.py
+++ cozapraci/jobs_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView, CreateView,DetailView, FormView,L
 from jobs_app.models import Job
 from . import forms
 from random import randint
+from random import choice
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -28,7 +29,11 @@ class JobDetailView(DetailView):
     model = Job
 
 def random_view(request):
-    all_jobs = Job.objects.all()
-    random_job = all_jobs[randint(1,len(all_jobs)-1)]
+    #all_jobs = Job.objects.all()
+    #random_job = all_jobs[randint(1,len(all_jobs)-1)]
+
+    pks = Job.objects.values_list('pk', flat=True)
+    random_pk = choice(pks)
+    random_job = Job.objects.get(pk=random_pk)
     context = {"job" : random_job}
     return render(request, 'jobs_app/job_detail.html', context=context)
